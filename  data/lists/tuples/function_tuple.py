5cde9c26001f911511df25ade5a4b3023b5039ec
Manita Tamang <5tamam00@solent.ac.uk>
2021-10-28 11:08:40 +0100

    Added code to demonstrate a simple tuple.

---
+++  data/lists/tuples/function_tuple.py
@@ -0,0 +1,13 @@
+def likelihood():
+    likelihoods = (50, 38, 27, 99, 4)
+    return min(likelihoods), max(likelihoods)
+
+
+def run():
+    probabilities = likelihood()
+    print(f"Minimum likelihood of falling: {probabilities[0]}%")
+    print(f"Maximum likelihood of falling: {probabilities[0]}%")
+
+
+if __name__ == "__main__":
+    run()
